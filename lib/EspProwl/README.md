3135f1e33f2cb780a6755ed91cd289b348d0356e
Merlin Beutlberger <r@ndombyte.net>
2020-10-26 23:57:36 +0100

    Add prowl push, various changes

---
+++ lib/EspProwl/README.md
@@ -0,0 +1,13 @@
+# EspProwl
+
+Esp8266 or ESP32 (Arduino) to IOS Prowl notification service
+
+Sends a push notification to your iOS device via Prowl.
+
+ Go to
+ 
+    https://www.prowlapp.com/api_settings.php
+ 
+ to create an API key.
+ 
+ See the example RingTheRedPhone for more.
--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,10 @@
 
 #include <Arduino.h>
 #include <Wire.h>
-//#include <ESP8266WiFi.h>
 #include "SH1106Wire.h"
 #include "OLEDDisplayUi.h"
+#include <ESP8266WiFi.h>
+#include <EspProwl.h>
 
 // Copy over "_constants.h.example" to "_constants.h" and update it with values suitable
 //	for your network
@@ -17,10 +18,11 @@
 #define SHOT_BUTTON_PIN 12
 #define HEATING_BUTTON_PIN 13
 
+#define DEBUG 0
+
 SH1106Wire display(0x3c, I2C_SDA, I2C_SCL);
 OLEDDisplayUi ui ( &display );
 
-/*
 const char* ssid = WIFI_SSID;
 const char* password = WIFI_PASSWORD;
 WiFiClient espClient;
@@ -48,30 +50,35 @@ void setup_wifi() {
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
   WiFi.setAutoReconnect(true);
-}*/
+
+  EspProwl.begin();
+  EspProwl.setApiKey((char *)PROWL_KEY);
+  EspProwl.setApplicationName((char *)PROWL_APP);
+}
 
 int timerMode = 0; // 1 = shot timer; 2 = heating timer
 int slowAnimationState = 0;
 int fastAnimationState = 0;
 bool done = false;
+bool pushHeatingComplete = false;
 
 const int heatingTime = 900000; // Up to 15 min
 const int shotTimer = 30000; // Up to 30 sec
 const int slowAnimationDuration = 1000;
 const int fastAnimationDuration = 120;
 
-long timerStart = 0;
-long lastSlowAnimation = 0;
-long lastFastAnimation = 0;
-long doneSince = 0;
-long doneTillStandby = 0;
+unsigned long timerStart = 0;
+unsigned long lastSlowAnimation = 0;
+unsigned long lastFastAnimation = 0;
+unsigned long doneSince = 0;
+unsigned long doneTillStandby = 0;
 
 void timeFrame(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
   display->setTextAlignment(TEXT_ALIGN_CENTER_BOTH);
   display->setFont(Roboto_Mono_Light_14);
 
-  long now = millis();
-  long elapsedMs = now - timerStart;
+  unsigned long now = millis();
+  unsigned long elapsedMs = now - timerStart;
   if (now - lastFastAnimation > fastAnimationDuration) {
     fastAnimationState++;
     if (fastAnimationState > 1) {
@@ -133,11 +140,14 @@ void timeFrame(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16
       display->drawString(64, 12, textDecoration + " Heating " + textDecoration);
       display->setFont(Roboto_Thin_24);
 
-      if (elapsedMs >= heatingTime) {
+      if (elapsedMs > heatingTime) {
         if (!done) {
           done = true;
           doneSince = now;
           doneTillStandby = heatingTime;
+          pushHeatingComplete = true;
+          display->drawString(64, 40, "Complete");
+          break;
         }
         // Time's up: Flash!
         if (fastAnimationState == 0) {
@@ -159,19 +169,19 @@ void timeFrame(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16
   }
 }
 
-long volatile timeOfLastInterrupt = 0;
+unsigned long volatile timeOfLastInterrupt = 0;
 int volatile targedTimerMode = -1;
 bool volatile reset = false;
 
-const int minTimeBetweenInterrupts = 10000; // In microseconds
+const int minTimeBetweenInterrupts = 200;
 
 void handleButtonPush(int targetTimerMode) {
   noInterrupts();
-  long currentTime = micros();
+  unsigned long currentTime = millis();
   if ((currentTime - timeOfLastInterrupt) > minTimeBetweenInterrupts) {
     timeOfLastInterrupt = currentTime;
-    targedTimerMode = targetTimerMode;
     reset = true;
+    targedTimerMode = targetTimerMode;
   }
   interrupts();
 }
@@ -202,7 +212,7 @@ void setup() {
   ui.init();
   display.flipScreenVertically();
 
-  // setup_wifi();
+  setup_wifi();
 }
 
 void loop() {
@@ -216,33 +226,45 @@ void loop() {
       display.displayOn();
     }
   }
+
   if (reset) {
+    Serial.println("Reset");
     timerStart = millis();
     reset = false;
-  }
-  
-  if (timerMode == 0) {
-    delay(10);
-    return;
-  }
-  int remainingTimeBudget = ui.update();
-  if (remainingTimeBudget > 0) {
-    // You can do some work here
-    // Don't do stuff if you are below your
-    // time budget.
-    delay(remainingTimeBudget);
-  } else {
-    delay(1); // 1 ms delay for reasons
+    done = false;
+    doneSince = 0;
+    doneTillStandby = 0;
   }
 
   if (done) {
-    long now = millis();
+    const unsigned long now = millis();
     if (now - doneSince > doneTillStandby) {
       // Standby
       targedTimerMode = 0;
       done = false;
       doneSince = 0;
       doneTillStandby = 0;
+      return; // To force immediate targetTimerMode evaluation
     }
+  }
+
+  if (pushHeatingComplete) {
+    pushHeatingComplete = false;
+    int returnCode = EspProwl.push((char *)"", (char *)"Heating Complete", 0);
+    if (returnCode == 200) {
+      if (DEBUG) Serial.println("OK.");
+    } else {
+      if (DEBUG) Serial.print("Error. Server returned: ");
+      if (DEBUG) Serial.print(returnCode);
+    }
+  }
+
+  int remainingTimeBudget = ui.update();
+  if (DEBUG) Serial.println(remainingTimeBudget);
+  if (remainingTimeBudget > 0) {
+    // You can do some work here
+    // Don't do stuff if you are below your
+    // time budget.
+    delay(remainingTimeBudget);
   }
 }
